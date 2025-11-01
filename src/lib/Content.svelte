a764e56fbb12af2ad469c726f51baf7363842d3a
Mansurkhoja <mansur.atm@gmail.com>
2025-11-01 20:36:05 +0500

    add editor in content component

---
+++ src/lib/Content.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Editor from './components/Editor.svelte'
+	import { isSidebarOpen } from './ui'
+</script>
+
+<div class="content" class:hidden={$isSidebarOpen}>
+	<Editor />
+</div>
+
+<style>
+	.content {
+		flex-grow: 1;
+		padding: 80px 1rem 0.5rem;
+		position: sticky;
+		top: 0;
+	}
+
+	@media screen and (max-width: 900px) {
+		.content {
+			padding-bottom: calc(42px + 0.5rem);
+		}
+
+		.content.hidden {
+			display: none;
+		}
+	}
+</style>
