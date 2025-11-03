108861e567bd29e20520f2b015fffb882f6ce1d0
Mansurkhoja <mansur.atm@gmail.com>
2025-11-03 16:26:50 +0500

    added min height

--- src/lib/Content.svelte
+++ src/lib/Content.svelte
@@ -13,11 +13,13 @@
 		padding: 80px 1rem 0.5rem;
 		position: sticky;
 		top: 0;
+		min-height: calc(100svh - 80px - 0.5rem);
 	}
 
 	@media screen and (max-width: 900px) {
 		.content {
 			padding-bottom: calc(42px + 0.5rem);
+			min-height: calc(100svh - 80px - 42px - 0.5rem);
 		}
 
 		.content.hidden {
